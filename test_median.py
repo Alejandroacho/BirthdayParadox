843a47a390c8ad22fad4e2eb7b436a6594bffd66
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2021-11-03 23:29:20 +0100

    Errors examples added to test

--- test_median.py
+++ test_median.py
@@ -1,5 +1,8 @@
-from calculate_median import calculate_median, calculate_number_of_people
 import math
+import pytest
+
+from calculate_median import calculate_median, calculate_number_of_people
+
 
 def test_median_is_fine():
     iterations = 10000
@@ -7,3 +10,16 @@ def test_median_is_fine():
     expected_value = 23
     actual_value = calculate_median(iterations, number_of_people)
     assert expected_value == math.floor(actual_value)
+
+def test_raise_value_error_when_small_sample_is_passed_to_people_calculator():
+    with pytest.raises(ValueError):
+        calculate_number_of_people(2)
+
+def test_raise_type_error_when_no_int_passed_to_people_calculator():
+    with pytest.raises(TypeError):
+        calculate_number_of_people("test")
+
+def test_raise_type_error_when_no_int_passed_to_median_calculator():
+    with pytest.raises(TypeError):
+        calculate_median(2, "test")
+        calculate_median("test", 2)
