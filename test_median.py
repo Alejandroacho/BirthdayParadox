8ce95bebc190181475ad6495950f96dcddb108f0
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2021-11-03 23:20:10 +0100

    Test refacored in order of filename change

--- test_median.py
+++ test_median.py
@@ -1,9 +1,9 @@
-import cumple
+from calculate_median import calculate_median, calculate_number_of_people
 import math
 
 def test_median_is_fine():
     iterations = 10000
     number_of_people = 10000
     expected_value = 23
-    actual_value = cumple.calculate_median(iterations, number_of_people)
+    actual_value = calculate_median(iterations, number_of_people)
     assert expected_value == math.floor(actual_value)
