5a7338adf35b6e419104a98a5e9d2e43e1ca6e87
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2021-11-03 23:28:59 +0100

    Error raises added to main functions

--- calculate_median.py
+++ calculate_median.py
@@ -4,14 +4,19 @@ YEAR_DAYS = 365
 
 def calculate_number_of_people(number_of_people):
     birthdays = []
+    if type(number_of_people) != int:
+        raise TypeError("number_of_people must be an integer")
     for person_number in range(number_of_people):
         person_birthday = random.randint(1, YEAR_DAYS)
         if person_birthday not in birthdays:
             birthdays.append(person_birthday)
         elif person_birthday in birthdays:
             return person_number
+    raise ValueError("Can't find person number maybe due to a small statistic sample")
 
 def calculate_median(iterations, number_of_people):
+    if type(iterations) != int or type(number_of_people) != int:
+        raise TypeError("number_of_people must be an integer")
     sum_of_numbers_given = sum(calculate_number_of_people(number_of_people) \
                                for _ in range(iterations))
     median = sum_of_numbers_given / iterations
